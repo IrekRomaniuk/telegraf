a7d05807972c87c064a96c19af0897dc33a8c82e
irom77 <irek.romaniuk@gmail.com>
2016-12-29 10:13:09 -0500

    Fixed error: PASS ok

--- plugins/inputs/pan_qosthroughput/pan_qosthroughput_test.go
+++ plugins/inputs/pan_qosthroughput/pan_qosthroughput_test.go
@@ -5,7 +5,9 @@ import (
 	"testing"
 
 	"github.com/influxdata/telegraf/testutil"
-	//"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"fmt"
 )
 
 func mockGetHTML1(url string) (string, error) {
@@ -22,17 +24,26 @@ func TestGather(t *testing.T) {
 		HTML: mockGetHTML1,
 	}
 
-	p.Gather(&acc)
-	tags := map[string]string{"class": "3", "int": "ae1",}
-	fields := map[string]interface{}{
-		"qos_throughput": "130784",
-	}
+	err := p.Gather(&acc)
+	assert.NoError(t, err)
+	metric, ok := acc.Get("qos_throughput")
+	require.True(t, ok)
+	qos_throughput := metric.Fields["qos_throughput"]
+	fmt.Println(qos_throughput)
+	tags := map[string]string{"class": "7", "int": "ae1",}
+	fields := map[string]interface{}{}
+	fields["qos_throughput"] = qos_throughput
+	/*fields := map[string]interface{}{
+		"qos_throughput": "13",
+	}*/
+	/*assert.False(t, acc.HasMeasurement("qos_throughput"),
+		"Missing qos_throughput measurement")*/
 	acc.AssertContainsTaggedFields(t, "qos_throughput", fields, tags)
 }
 
 var HTMLThroughput1 = `<response status="success">
 <result>
-Class 1 0 kbps Class 2 0 kbps Class 3 0 kbps Class 4 130784 kbps Class 5 0 kbps Class 6 0 kbps Class 7 20 kbps Class 8 12 kbps
+Class 1 0 kbps Class 2 0 kbps Class 3 0 kbps Class 4 130784 kbps Class 5 0 kbps Class 6 0 kbps Class 7 20 kbps Class 8 13 kbps
 </result>
 </response>`
 
@@ -43,7 +54,7 @@ Class 4         130784 kbps
 Class 5              0 kbps
 Class 6              0 kbps
 Class 7             20 kbps
-Class 8             12 kbps
+Class 8             13 kbps
 </result></response>`
 
 
